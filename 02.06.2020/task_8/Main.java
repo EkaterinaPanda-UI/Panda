3117e4ca325c8cdf1fcf555165f0bd475a9d6ab1
EkaterinaPanda-UI <66119450+EkaterinaPanda-UI@users.noreply.github.com>
2020-06-02 18:08:36 +0300

    Create Main.java

---
+++ 02.06.2020/task_8/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        for (int a = 1; a<=10; a++){
+            for (int b = 0; b<a; b++){
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+    }
+}
