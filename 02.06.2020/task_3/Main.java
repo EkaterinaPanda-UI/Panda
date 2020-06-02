1eafbbc432660c9d036c35f4809b44f7b000483c
EkaterinaPanda-UI <66119450+EkaterinaPanda-UI@users.noreply.github.com>
2020-06-02 16:17:01 +0300

    Create Main.java

---
+++ 02.06.2020/task_3/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static int multiplication(int num) {
+        int result = 1;
+        for (int i = 1; i <= num; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println(multiplication(10));
+
+    }
+}
